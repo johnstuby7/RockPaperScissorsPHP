84ff0f6134d160aa4b9da101c28cdeb906e4e09c
johnstuby7 <johnstuby7@gmail.com>
2021-05-05 11:27:53 -0600

    Pushing up rockpaperscissors game to github

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<title>John Stuby 5a1d2105 </title>
+	<?php require_once "bootstrap.php"; ?>
+	</head>
+	<body>
+		<div class="container">
+			<h1>Welcome to Rock Paper Scissors</h1>
+			<a href="login.php">Please Log In</a>
+			<p>
+			Attempt to go to 
+			<a href="game.php">game.php</a> without logging in - it should fail with an error message.
+		</div>
+	</body>
+</html>
